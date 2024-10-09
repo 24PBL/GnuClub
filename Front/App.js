5c3a4ff5f15937b3e6eea33e47d0eeb83a1fb4f1
24PBL <ijshthat24@gmail.com>
2024-10-09 15:51:09 +0900

    로그인 구현중

--- Front/App.js
+++ Front/App.js
@@ -11,14 +11,22 @@ import SignUpScreens from './src/screens/Signup/SignUp';
 const RootStack = createNativeStackNavigator()
 
 export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <NavigationContainer>
       <RootStack.Navigator>
-        <RootStack.Screen name="Home" options={{headerShown : false}} component={HomeScreens}/>
+        {!isSignedIn ? (
+        <>
+        <RootStack.Screen name="SignUp" options={{headerShown : false}} component={SignUpScreens}/>
         <RootStack.Screen name="Login" options={{headerShown : false}} component={LoginScreens}/>
+        </>
+        ) : (
+          <>
+        <RootStack.Screen name="Home" options={{headerShown : false}} component={HomeScreens}/>
         <RootStack.Screen name="Verification" options={{headerShown : false}} component={VerificationScreens}/>
         <RootStack.Screen name="Profile" options={{headerShown : false}} component={ProfileScreens} />
-        <RootStack.Screen name="SignUp" options={{headerShown : false}} component={SignUpScreens}/>
+        </>
+        )}
       </RootStack.Navigator>
      </NavigationContainer>
   );
