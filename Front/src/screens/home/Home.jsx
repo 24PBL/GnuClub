d8e9de10afbdab352efc0077fecc117b04021ae4
24PBL <ijshthat24@gmail.com>
2024-10-09 16:06:45 +0900

    홈 스크린 수정

--- Front/src/screens/home/Home.jsx
+++ Front/src/screens/home/Home.jsx
@@ -9,79 +9,11 @@ function MainScreen() {
   return (
     <Back>
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>홈 화면 예정</Text>{/*커밋 확인용 주석*/} {/*원격접속 커밋 확인용 주석 */}
+      <Text>홈 화면 예정</Text>
     </View>
     </Back>
   );
 }
-    const goToLoginScreens = () => {
-      navigation.navigate("Login")
-      console.log("로그인 화면으로 이동");
-    }
-    return (
-        <SafeAreaView>
-          <StoryList horizontal={true} showsHorizontalScrollIndicator={false}>
-            <Story onPress={goToLoginScreens}>
-              <Image 
-                source={{
-                  uri:""}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <Ionicons name='chatbubble-ellipses-outline' size={40} color={'black'}/>
-              <UserName>react</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:"."}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>테스트로 다시 바꿈</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:"."}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>리액트</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:"."}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>테스트</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:""}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>리액트</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:""}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>리액트</UserName>
-            </Story>
-            <Story>
-              <Image 
-                source={{
-                  uri:"."}}
-                style={{width:80, height: 80, borderRadius:40}}
-                />
-              <UserName>리액트</UserName>
-            </Story>
-          </StoryList>
-        </SafeAreaView>
-    );
-  
 
 
 function ClubScreens(){
